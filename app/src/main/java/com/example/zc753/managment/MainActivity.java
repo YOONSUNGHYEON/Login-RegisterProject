f8c0490b667d58848bd095d1ef4c030fdbbaeceb
본인 GitHub 아이디[출처] <qead753951@naver.com>
2018-10-07 22:50:35 +0900

    the sixth lecture commit

--- app/src/main/java/com/example/zc753/managment/MainActivity.java
+++ app/src/main/java/com/example/zc753/managment/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.zc753.managment;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         TextView idText = (TextView)findViewById(R.id.idText);
         TextView passwordText = (TextView)findViewById(R.id.passwordText);
         TextView welcomeMessage = (TextView)findViewById(R.id.welcomeMessage);
+        Button managementButton = (Button) findViewById(R.id.managmentButton);
+
 
         Intent intent  = getIntent(); //인텐트값 받기
         String userID = intent.getStringExtra("userID");
@@ -26,5 +37,64 @@ public class MainActivity extends AppCompatActivity {
         idText.setText(userID);
         passwordText.setText(userPassword);
         welcomeMessage.setText(message);
+
+        if(!userID.equals("admin"))
+        {
+            managementButton.setVisibility(View.GONE);
+        }
+        managementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new BackgroundTask().execute();
+            }
+        });
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String>
+    {
+        String target;
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                URL url= new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected  void onPreExecute(){
+            target = "http://zc753951.cafe24.com/List.php";
+        }
+
+
+
+        @Override
+        public  void onProgressUpdate(Void... values){
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        public void onPostExecute(String result){
+            Intent intent = new Intent(MainActivity.this, ManagementActivity.class);
+            intent.putExtra("userList", result);
+            MainActivity.this.startActivity(intent);
+        }
     }
 }
